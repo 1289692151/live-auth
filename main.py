748d66de39a43acf9b3cdd10b2c9bf2c65ffc3b0
1289692151 <114651263+1289692151@users.noreply.github.com>
2026-06-10 14:44:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -362,6 +362,7 @@ def step5_submit_real_name(log, login_token, id_card, real_name, proxy_api=None,
                             headers=headers, json=payload, timeout=15, verify=False)
         result = resp.json()
         code = result.get("code")
+        return False, True
         if code == "OK" and result.get("data", {}).get("status") == "pass":
             log("✅ 实名认证通过！")
             return True, False
@@ -1149,6 +1150,7 @@ class MainScreen(BoxLayout):
 
     @mainthread
     def _show_find_popup(self, id_card):
+        self.log("🔔 准备弹出找回确认窗口")
         # 防止重复弹窗
         if self._find_popup:
             try:
